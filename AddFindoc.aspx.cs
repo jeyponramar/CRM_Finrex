44b1acde831715d81958df327b2178bbcf418a32
inbaseelanseelan <inbaseelanseelan@gmail.com>
2024-11-22 14:17:05 +0530

    findoc changes

---
+++ AddFindoc.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using WebComponent;
+public partial class AddFindoc : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        int clientId = GlobalUtilities.ConvertToInt(CustomSession.Session("Login_ClientId"));
+        int clientUserId = GlobalUtilities.ConvertToInt(CustomSession.Session("Login_ClientUserId"));
+        Hashtable hstbl = new Hashtable();
+        hstbl.Add("findocdepartmentid", txtfindocdepartmentid.Text);
+        hstbl.Add("findoccategoryid", txtfindoccategoryid.Text);
+        hstbl.Add("findocsubcategoryid", txtfindocsubcategoryid.Text);
+        hstbl.Add("findocdocumenttypeid", txtfindocdocumenttypeid.Text);
+        hstbl.Add("clientid",clientId);
+        hstbl.Add("clientuserid",clientUserId);
+        hstbl.Add("subject", txtsubject.Text);
+        hstbl.Add("uploaddate", "getdate()");
+        InsertUpdate obj = new InsertUpdate();
+        int findocdocumentId = obj.InsertData(hstbl, "tbl_findocdocument");
+        if (findocdocumentId > 0)
+        {
+            lblmessage.Text = "Data saved successfully";
+        }
+
+    }
+}
---
+++ findocsubcategory/add.aspx.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebComponent;
+using System.Data;
+using System.Collections;
+using System.Data.SqlClient;
+using System.Web.UI.HtmlControls;
+using System.Text;
+
+public partial class FinDocSubCategory_add : System.Web.UI.Page
+{
+    GlobalData gblData = new GlobalData("tbl_findocsubcategory", "findocsubcategoryid");
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //FillDropDown_START
+			
+			EnableControlsOnEdit();
+			//FillDropDown_END
+            if (Request.QueryString["id"] == null)
+            {
+       			//PopulateAcOnAdd_START
+			
+			//PopulateAcOnAdd_END
+				//PopulateOnAdd_START
+				DataRow drpop = CommonPage.PopulateOnAdd(form);//pop=true&popm=&popjoin=&popid=
+				//PopulateOnAdd_END
+                //SetDefault_START//SetDefault_END
+            }
+            else
+            {
+                //Populate_START
+                gblData.PopulateForm(form, GetId());
+                //Populate_END
+            }
+            //CallPopulateSubGrid_START
+			
+			//CallPopulateSubGrid_END
+        }
+        //PageTitle_START
+        if (Request.QueryString["id"] == null)
+        {
+            lblPageTitle.Text = "Add FinDoc Sub Category";
+        }
+        else
+        {
+            lblPageTitle.Text = "Edit FinDoc Sub Category";
+        }
+        ClientScript.RegisterClientScriptBlock(typeof(Page), "title", "<script>setTitle('" + lblPageTitle.Text + "')</script>");
+        //PageTitle_END
+    }
+    //PopulateSubGrid_START
+			
+			//PopulateSubGrid_END
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        SaveData(true);
+    }
+    private int SaveData(bool isclose)
+    {
+        lblMessage.Visible = false;
+        //SetCode_START//SetCode_END
+        //ExtraValues_START//ExtraValues_END
+        int id = 0;
+        int clientid = GlobalUtilities.ConvertToInt(txtclientid.Text);
+        string query = @"select * from tbl_findocsubcategory where findocsubcategory_clientid=@clientid 
+                         and findocsubcategory_subcategoryname=@subcategoryname";
+        Hashtable hstblparam = new Hashtable();
+        hstblparam.Add("clientid", clientid);
+        hstblparam.Add("subcategoryname", txtsubcategoryname.Text);
+        InsertUpdate obj = new InsertUpdate();
+        DataRow dr = obj.ExecuteSelectRow(query, hstblparam);
+        if (dr != null)
+        {
+            lblMessage.Text = "Dublicate subcategory name not allowed";
+            lblMessage.Visible = true;
+            return 0;
+        }
+        id = gblData.SaveForm(form, GetId());
+
+        if (id > 0)
+        {
+            //SaveSubTable_START
+			
+			//SaveSubTable_END
+            //SaveFile_START
+			//SaveFile_END
+            //ParentCountUpdate_START
+			
+			//ParentCountUpdate_END
+            lblMessage.Text = "Data saved successfully!";
+            lblMessage.Visible = true;
+            CommonPage.CloseQuickAddEditWindow(Page, form, id);
+        }
+        else if (id == -1)
+        {
+            lblMessage.Text = "Data already exists, duplicate entry not allowed!";
+            lblMessage.Visible = true;
+        }
+        else
+        {
+            lblMessage.Text = "Error occurred while saving data</br>Error : " + gblData._error;
+            lblMessage.Visible = true;
+        }
+        return id;
+    }
+    //EnableControlsOnEdit_START
+	private void EnableControlsOnEdit()
+	{
+		if (Request.QueryString["id"] == null) return;
+		btnDelete.Visible = true;
+	}//EnableControlsOnEdit_END
+    private int GetId()
+    {
+        if (h_IsCopy.Text == "1")
+        {
+            return 0;
+        }
+        else
+        {
+            return GlobalUtilities.ConvertToInt(Request.QueryString["id"]);
+        }
+    }
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        Common.Delete();
+    }  
+    
+}
